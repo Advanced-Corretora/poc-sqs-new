1bf92cd365b36c660fb89da218b5f13eaa3afd88
Felipe Alexandre <felipeomachado@hotmail.com>
2024-11-11 19:46:20 +0000

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Mudanca 99999999999999 Hello World from: ${process.env.AWS_REGION} | -> ${process.env.INTEGRATION_REGISTER_SERVICE_BASE_PATH}`;
+    return `Mudanca Gabtech Hello World from: ${process.env.AWS_REGION} | -> ${process.env.INTEGRATION_REGISTER_SERVICE_BASE_PATH}`;
   }
 }
